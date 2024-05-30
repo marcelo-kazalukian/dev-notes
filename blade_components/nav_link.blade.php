911332b57adc4fd038f10bd36465acb97962b938
marcelo-kazalukian <marcelok1985@gmail.com>
2024-05-30 13:30:14 +1000

    Create nav_link.blade.php

---
+++ blade_components/nav_link.blade.php
@@ -0,0 +1,13 @@
+```blade
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'rounded text-primary-700 dark:text-primary-500'
+            : 'text-gray-700 hover:text-primary-700 dark:text-gray-400 dark:hover:text-white';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes . 'block']) }}>
+    {{ $slot }}
+</a>
+```
