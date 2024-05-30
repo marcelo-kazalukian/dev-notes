5cbe135c81baf1cb8f6764bfc017636255e089f1
marcelo-kazalukian <marcelok1985@gmail.com>
2024-05-30 13:32:38 +1000

    Update nav_link.blade.php

--- blade_components/nav_link.blade.php
+++ blade_components/nav_link.blade.php
@@ -6,6 +6,6 @@ $classes = ($active ?? false)
             : 'text-gray-700 hover:text-primary-700 dark:text-gray-400 dark:hover:text-white';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes . 'block']) }}>
+<a {{ $attributes->merge(['class' => $classes . ' block']) }}>
     {{ $slot }}
 </a>
